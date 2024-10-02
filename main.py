b1e53624c03e6e02ef567b06a98a5e5631cb0372
Sahil Parkash <sahiloutlook1962@gmail.com>
2024-10-02 23:07:56 +0500

    Summary of Changes: Removed the DataTable. Automatically defaults to showing the last 7 days on app load. Excludes today's data from the displayed workouts.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
-from dash.dash_table import DataTable
 import json
 from whoop import WhoopClient
 import datetime
@@ -82,38 +81,6 @@ app.layout = html.Div(style={'display': 'flex', 'flexDirection': 'column', 'minH
                 type="default",
                 children=[
                     html.Div(id="output-workouts", style={'textAlign': 'center', 'marginBottom': '20px'}),
-                    DataTable(
-                        id="workouts-table",
-                        columns=[],
-                        data=[],
-                        style_table={
-                            'overflowX': 'auto',
-                            'overflowY': 'auto',
-                            'height': '300px',
-                            'border': 'thin lightgrey solid',
-                            'borderRadius': '5px',
-                            'boxShadow': '0 2px 5px rgba(0, 0, 0, 0.1)',
-                            'marginBottom': '20px',
-                        },
-                        style_cell={
-                            'textAlign': 'left',
-                            'padding': '10px',
-                            'border': '1px solid lightgrey',
-                        },
-                        style_header={
-                            'backgroundColor': '#34495e',
-                            'color': 'white',
-                            'fontWeight': 'bold',
-                        },
-                        style_data_conditional=[
-                            {
-                                'if': {'row_index': 'odd'},
-                                'backgroundColor': '#f9f9f9',
-                            },
-                        ],
-                        page_size=10,
-                        style_as_list_view=True,
-                    ),
                     dcc.Graph(id="strain-chart", style={'border': 'thin lightgrey solid', 'borderRadius': '5px'}),
                 ]
             ),
@@ -121,11 +88,9 @@ app.layout = html.Div(style={'display': 'flex', 'flexDirection': 'column', 'minH
     ]),
 ])
 
-# Callback to fetch workouts based on start date and prepare data for the table and chart
+# Callback to fetch workouts based on start date and prepare data for the chart
 @app.callback(
     Output("output-workouts", "children"),
-    Output("workouts-table", "data"),
-    Output("workouts-table", "columns"),
     Output("strain-chart", "figure"),
     Input("7-days-btn", "n_clicks"),
     Input("14-days-btn", "n_clicks"),
@@ -142,73 +107,60 @@ def fetch_workouts(n7, n14, n30, n3m, n6m, n_all, n_custom, start_date, end_date
     ctx = dash.callback_context
 
     if not ctx.triggered:
-        return "", [], [], go.Figure()
-
-    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-    # Calculate the start date based on the button clicked
-    if button_id == '7-days-btn':
         start_date = (datetime.date.today() - datetime.timedelta(days=7)).strftime("%Y-%m-%d")
-    elif button_id == '14-days-btn':
-        start_date = (datetime.date.today() - datetime.timedelta(days=14)).strftime("%Y-%m-%d")
-    elif button_id == '30-days-btn':
-        start_date = (datetime.date.today() - datetime.timedelta(days=30)).strftime("%Y-%m-%d")
-    elif button_id == '3-months-btn':
-        start_date = (datetime.date.today() - datetime.timedelta(days=90)).strftime("%Y-%m-%d")
-    elif button_id == '6-months-btn':
-        start_date = (datetime.date.today() - datetime.timedelta(days=180)).strftime("%Y-%m-%d")
-    elif button_id == 'all-time-btn':
-        start_date = "2020-01-01"
-    elif button_id == 'submit-btn':
-        # Use the custom date range from the DatePicker
-        start_date = start_date
-        end_date = end_date
+        end_date = datetime.date.today().strftime("%Y-%m-%d")
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-    # Fetch workouts and create table and chart (same logic as before)
+        # Calculate the start date based on the button clicked
+        if button_id == '7-days-btn':
+            start_date = (datetime.date.today() - datetime.timedelta(days=7)).strftime("%Y-%m-%d")
+        elif button_id == '14-days-btn':
+            start_date = (datetime.date.today() - datetime.timedelta(days=14)).strftime("%Y-%m-%d")
+        elif button_id == '30-days-btn':
+            start_date = (datetime.date.today() - datetime.timedelta(days=30)).strftime("%Y-%m-%d")
+        elif button_id == '3-months-btn':
+            start_date = (datetime.date.today() - datetime.timedelta(days=90)).strftime("%Y-%m-%d")
+        elif button_id == '6-months-btn':
+            start_date = (datetime.date.today() - datetime.timedelta(days=180)).strftime("%Y-%m-%d")
+        elif button_id == 'all-time-btn':
+            start_date = "2020-01-01"
+        elif button_id == 'submit-btn':
+            # Use the custom date range from the DatePicker
+            start_date = start_date
+            end_date = end_date
+
+    # Fetch workouts and create chart
     try:
         with WhoopClient(USERNAME, PASSWORD) as client:
             workouts = client.get_cycle_collection(start_date=start_date)
     except Exception as e:
-        return f"Error fetching workouts: {str(e)}", [], [], go.Figure()
+        return f"Error fetching workouts: {str(e)}", go.Figure()
+
+    # Filter out today's data
+    workouts = [workout for workout in workouts if workout.get("start")[:10] != datetime.date.today().strftime("%Y-%m-%d")]
 
     # Save workouts to a JSON file
     with open("workouts.json", "w") as f:
         json.dump(workouts, f, indent=4)
 
-    # Prepare data for DataTable and strain chart
-    workout_data = []
+    # Prepare data for strain chart
     strain_scores = []
     dates = []
 
     for workout in workouts:
         score = workout.get("score")  # Get the score dictionary
         strain = score.get("strain") if score else None
-        workout_data.append({
-            "ID": workout.get("id"),
-            "User ID": workout.get("user_id"),
-            "Created At": workout.get("created_at"),
-            "Updated At": workout.get("updated_at"),
-            "Start": workout.get("start"),
-            "End": workout.get("end"),
-            "Timezone Offset": workout.get("timezone_offset"),
-            "Score State": workout.get("score_state"),
-            "Strain": strain,
-            "Average Heart Rate": score.get("average_heart_rate") if score else None,
-            "Max Heart Rate": score.get("max_heart_rate") if score else None,
-            "Kilojoules": score.get("kilojoule") if score else None,
-    })
         strain_scores.append(strain)
         dates.append(workout.get("start"))
 
-    columns = [{"name": i, "id": i} for i in workout_data[0].keys()] if workout_data else []
-    
     # Prepare strain chart
     strain_chart = go.Figure(
         data=[go.Scatter(x=dates, y=strain_scores, mode='lines+markers')],
         layout=go.Layout(title="Strain Scores Over Time", xaxis={'title': 'Date'}, yaxis={'title': 'Strain Score'})
     )
 
-    return f"Fetched {len(workouts)} workouts.", workout_data, columns, strain_chart
+    return f"Fetched {len(workouts)} workouts.", strain_chart
 
 # Run the Dash app with Waitress if this script is run directly
 serve(app.server, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
